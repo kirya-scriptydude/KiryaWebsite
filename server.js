2add5832ee7eaeb48b5ab1fad9db12aa15579328
Kirill <scriptythememer@gmail.com>
2024-04-09 14:10:07 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//npm run devStart
+
+const express = require("express");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.listen(3000);
