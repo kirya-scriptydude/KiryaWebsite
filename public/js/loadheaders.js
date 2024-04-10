a24f03b1a8a2f0f71f8c9223422bdb3b23410e0a
Kirill <scriptythememer@gmail.com>
2024-04-10 14:56:52 +0700

    loadheaders.js for loading a header.

---
+++ public/js/loadheaders.js
@@ -0,0 +1,11 @@
+let appHeader = `
+<div class="infoHeader">
+    <p class="logo">Whatever whatever</p>
+    <div class="headerRight">
+        <a href="/works">Works</a>
+        <a href="/contact">Contact</a>
+    </div>
+</div>
+`;
+
+document.getElementById("website-header").innerHTML = appHeader;
