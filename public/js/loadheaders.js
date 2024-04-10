705d8ca681c4fe1ac41d514057a151a0c7d8feba
Kirill <scriptythememer@gmail.com>
2024-04-10 17:27:47 +0700

    cool navbar improvements

--- public/js/loadheaders.js
+++ public/js/loadheaders.js
@@ -1,8 +1,9 @@
 let appHeader = `
 <div class="infoHeader">
-    <p class="logo">Whatever whatever</p>
+    <p class="logo">kirya.com</p>
     <div class="headerRight">
         <a href="/works">Works</a>
+        <a href="">Commisions</a>
         <a href="/contact">Contact</a>
     </div>
 </div>
