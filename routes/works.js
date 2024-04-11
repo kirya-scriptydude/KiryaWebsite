0d0ba2525b662965b91b0e32ad2961dddd70799e
Kirill <scriptythememer@gmail.com>
2024-04-11 14:48:54 +0700

    works placeholder

--- routes/works.js
+++ routes/works.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("works");
+    const works = [{
+        title: "Test Article",
+        date: Date.now(),
+        description: "This is a test article woah"
+    }]
+
+    res.render("works", { works: works });
 })
 
 module.exports = router;
