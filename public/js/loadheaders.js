5335ecd36970304933960ecc864e755258210ba2
Kirill <scriptythememer@gmail.com>
2024-04-11 12:43:02 +0700

    fonts + index content

--- public/js/loadheaders.js
+++ public/js/loadheaders.js
@@ -1,6 +1,6 @@
 let appHeader = `
 <div class="infoHeader">
-    <p class="logo">kirya.com</p>
+    <a href="/" class="logo">kirya.com</p>
     <div class="headerRight">
         <a href="/works">Works</a>
         <a href="">Commisions</a>
