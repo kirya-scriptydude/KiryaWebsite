92d1e00a92eea8f779bb3c94b90b7d4e49c191c5
Kirill <scriptythememer@gmail.com>
2024-04-15 17:59:21 +0700

    workpost.ejs (you can view works now)

--- routes/works.js
+++ routes/works.js
@@ -8,4 +8,15 @@ router.get("/", async (req, res) => {
     res.render("works", { works: works });
 })
 
+router.get("/:id", async (req, res) => {
+    const workArticle = await workScheme.find().where({dirname: req.params.id});
+    console.log(workArticle);
+
+    if (workArticle.length != 0) {
+        res.render("posts/workpost", {workArticle: workArticle[0]})
+    } else {
+        res.status(404).send("404 Not Found");
+    }
+})
+
 module.exports = router;
