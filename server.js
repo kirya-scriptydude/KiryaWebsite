4417f0fc162bbb3b59b412058eca4be4c35185ae
Kirill <scriptythememer@gmail.com>
2024-04-16 23:23:17 +0700

    changing method for serverside table inserting

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 mongoose.connect(DATABASE_URL);
+const workScheme = require("./models/work_post");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+
+async function saveNewForm(postToSave) {
+    await postToSave.save();
+}
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -22,3 +27,20 @@ app.get("/contact", (req, res) => {
 })
 
 app.listen(3000);
+
+
+
+//COMMENT OUT STUFF WHEN NOT IN NEED OF SAVING STUFF
+const workpost = new workScheme({
+    dirname: "test-work-work",
+    title: "",
+    description: "",
+    content: `
+    # THIS IS REAL HOOD CLASSIC
+    ## YEAH
+    yeah shututp `,
+
+    imageDir: ""
+});
+
+//saveNewForm(workpost);
