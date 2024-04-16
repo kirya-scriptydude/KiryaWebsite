f9da3fd90dfd7d1779b22c927d9f8cf75198ad30
Kirill <scriptythememer@gmail.com>
2024-04-16 22:57:38 +0700

    markdown support + a little bit cleaner ui

--- models/work_post.js
+++ models/work_post.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+const createDomPurifier = require("dompurify");
+const { JSDOM } = require("jsdom");
+const { marked } = require("marked");
+const dompurify = createDomPurifier(new JSDOM().window);
+
 const workpostSchema = new mongoose.Schema({
     dirname: {
         type: String,
@@ -22,7 +27,21 @@ const workpostSchema = new mongoose.Schema({
     imageDir: {
         type: String,
         default: "/images/placeholder.png"
+    },
+
+    sanitizedHtml: {
+        type: String,
+        required: true
     }
+})
+
+workpostSchema.pre("validate", function(next) {
+
+    if (this.content) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.content));
+    }
+
+    next();
 })
 
 module.exports = mongoose.model("WorksPost", workpostSchema);
